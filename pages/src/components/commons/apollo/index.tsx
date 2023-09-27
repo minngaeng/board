a4c5bc30469a9c10209ad10a8fc371280df303ba
minngaeng <minngaeng@gmail.com>
2023-09-27 18:32:48 +0900

    createUploadLink settings

--- pages/src/components/commons/apollo/index.tsx
+++ pages/src/components/commons/apollo/index.tsx
@@ -1,12 +1,22 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
 
 interface IApolloSettingProps {
   children: JSX.Element;
 }
 
 export default function ApolloSetting(props: IApolloSettingProps) {
-  const client = new ApolloClient({
+  const uploadLink = createUploadLink({
     uri: 'http://backend-practice.codebootcamp.co.kr/graphql',
+  });
+
+  const client = new ApolloClient({
+    link: ApolloLink.from([uploadLink]),
     cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장
   });
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
